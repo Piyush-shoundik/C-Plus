4f082993def05641894f6e189b007abe27178213
Piyush-shoundik <ritetopiyush@gmail.com>
2025-07-28 15:53:12 +0530

    use std: : namespace

--- firstcode.cpp
+++ firstcode.cpp
@@ -22,18 +22,37 @@
 
 // adding two no.
 
-#include <iostream>
-using namespace std; 
+// #include <iostream>
+// using namespace std; 
 
+// main()
+// {
+//     int a,b,c;
+//     cout << "Enter your first no. ";
+//     cin >> a;
+//     cout << "Enter your second no. ";
+//     cin >> b;
+//     c = a + b;
+//     cout << "This is the sum " << c;  
+
+// return 0; 
+// }
+
+// trying for one more time 
+
+# include <iostream>
 main()
 {
-    int a,b,c;
-    cout << "Enter your first no. ";
-    cin >> a;
-    cout << "Enter your second no. ";
-    cin >> b;
-    c = a + b;
-    cout << "This is the sum " << c;  
-    
-return 0; 
+    int a,b,c,d,e;
+    std:: cout << "Enter first no. you want to add ";
+    std:: cin >> a;
+    std:: cout << "Enter second no. you want to add ";
+    std:: cin >> b;
+    std:: cout << "Enter first no. you want to subtract ";
+    std:: cin >> c;
+    std:: cout << "Enter second no. you want to subtract ";
+    std:: cin >> d;
+    e = a + b - c - d ;
+    std:: cout << "This is the result of your calculation " << e ;
+
 }
