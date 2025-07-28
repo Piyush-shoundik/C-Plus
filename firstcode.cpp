d15eb414f34d5b5fbfe7062ab419f0f7e156b61a
Piyush-shoundik <ritetopiyush@gmail.com>
2025-07-28 15:13:59 +0530

    first program to add two int no.

--- firstcode.cpp
+++ firstcode.cpp
@@ -10,20 +10,30 @@
 // #include <iostream>
 // using namespace std;
 
-int main()
-{
-    int a;
-    cout << "Enter a number: ";
-    cin >> a;
-    cout << "You entered: " << a << endl;
-    return 0;
-}
-
-// #include <iostream>
-// using namespace std; 
-
-// main()
+// int main()
 // {
-//     int a,b,c
-// return 0; 
+//     int a;
+//     cout << "Enter a number: ";
+//     cin >> a;
+//     cout << "You entered: " << a << endl;
+//     return 0;
 // }
+
+
+// adding two no.
+
+#include <iostream>
+using namespace std; 
+
+main()
+{
+    int a,b,c;
+    cout << "Enter your first no. ";
+    cin >> a;
+    cout << "Enter your second no. ";
+    cin >> b;
+    c = a + b;
+    cout << "This is the sum " << c;  
+    
+return 0; 
+}
