dcb4565660aa74df31a369b7d85f2128bfbe6efa
Piyush-shoundik <ritetopiyush@gmail.com>
2026-02-15 22:03:13 +0530

    one more method for  is insertion sort

--- firstcode.cpp
+++ firstcode.cpp
@@ -4120,33 +4120,61 @@
 
 // insertion sort in a function
 
-#include <iostream> 
-#include <vector>
+// #include <iostream> 
+// #include <vector>
+// using namespace std;
+
+
+// void sort(int arr[], int n){
+//     for(int i = 1; i < n ; i++){
+//         int temp = arr[i];
+//         int j = i - 1; 
+
+//         while(j >= 0 && temp < arr[j]){
+//             arr[j+1] = arr[j];
+//             j--;
+//         }
+
+
+//         arr[j+1] = temp;
+//     }
+// }
+
+// int main(){
+//     int arr[] = {2,4,5,1,3};
+//     int size = 5;
+
+//     sort(arr,size);
+
+//     for(int i = 0; i < size; i++){
+//         cout << arr[i] << " ";
+//     }
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// using insertion sort
+
+#include <iostream>
 using namespace std;
 
+int main(){
+    int arr[] = {5,4,2,3,1};
+    int n = 5;
 
-void sort(int arr[], int n){
     for(int i = 1; i < n ; i++){
-        int temp = arr[i];
-        int j = i - 1; 
-
-        while(j >= 0 && temp < arr[j]){
+        int temp = arr[i]; //5
+        int j = i-1; //1
+        while(arr[j] > temp && j >= 0 ){
             arr[j+1] = arr[j];
             j--;
         }
-
-        
         arr[j+1] = temp;
     }
-}
 
-int main(){
-    int arr[] = {2,4,5,1,3};
-    int size = 5;
-
-    sort(arr,size);
-
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
+    return 0;
+    
 }
